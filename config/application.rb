068c7ccc0f635b1ff4bb8d04d3e2bfeabd59e447
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2014-02-19 15:35:20 -0300

    even if disabling ConditionalGet and ETag middlewares

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Streaming
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.middleware.delete 'Rack::ConditionalGet'
+    config.middleware.delete 'Rack::ETag'
   end
 end

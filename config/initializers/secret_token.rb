35208384cf6033a45ce60cf0e64648d632298485
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2014-02-19 15:06:33 -0300

    rails _4.0.3_ new streaming -O -T --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Streaming::Application.config.secret_key_base = '83048a4ee8b8a725b474c0dd578185868c9ffb7aa6b65faddaed08094ec9743a62ea17bc30a91edc4bad225c0da50a7d0844e7c2889ce4d5ab5d3bb29a1a623a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Streaming::Application.config.session_store :cookie_store, key: '_streaming_session'
